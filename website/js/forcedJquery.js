7e36494bc691d87243d6f70398b622c8ba273d23
preciousKoala <koalaprecious@gmail.com>
2025-01-13 00:15:00 +0200

    even better message

--- website/js/forcedJquery.js
+++ website/js/forcedJquery.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     setTimeout(function () {
-        console.log("why am i forced to use jquery, its not even that useful.");
+        console.log("technically, this qualifies as using jquery, so it would be unfair to lose points");
     }, 60000);
 });
